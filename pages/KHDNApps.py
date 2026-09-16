b7abed8af95f8e169a23698e9beebef76081acb2
luisnghia <nghia1214@gmail.com>
2026-09-16 15:08:22 +0700

    KHDN V2.31.3: enable full task editor in embedded page

--- pages/KHDNApps.py
+++ pages/KHDNApps.py
@@ -24,8 +24,10 @@ import khdn_apps.cbht_workload_patch as _workload_patch_module  # noqa: E402
 from khdn_apps.cbht_workload_patch import install as _install_v231  # noqa: E402
 from khdn_apps.leader_workload_match_patch import install as _install_v2311  # noqa: E402
 from khdn_apps.amount_decimal_patch import install as _install_v2312  # noqa: E402
+from khdn_apps.full_task_edit_patch import install as _install_v2313  # noqa: E402
 
 _install_v231(_app_module.__dict__)
 _install_v2311(_app_module.__dict__, _workload_patch_module)
 _install_v2312(_app_module.__dict__)
+_install_v2313(_app_module.__dict__)
 _app_module.app()
