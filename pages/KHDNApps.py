6038f2e48c966a2e2d07e39e55239596d8687e83
luisnghia <nghia1214@gmail.com>
2026-09-16 15:40:51 +0700

    KHDN V2.31.4: install direct task edit button in embedded page

--- pages/KHDNApps.py
+++ pages/KHDNApps.py
@@ -21,13 +21,16 @@ if str(ROOT) not in sys.path:
 
 import khdn_apps.app as _app_module  # noqa: E402
 import khdn_apps.cbht_workload_patch as _workload_patch_module  # noqa: E402
+import khdn_apps.full_task_edit_patch as _full_task_edit_module  # noqa: E402
 from khdn_apps.cbht_workload_patch import install as _install_v231  # noqa: E402
 from khdn_apps.leader_workload_match_patch import install as _install_v2311  # noqa: E402
 from khdn_apps.amount_decimal_patch import install as _install_v2312  # noqa: E402
 from khdn_apps.full_task_edit_patch import install as _install_v2313  # noqa: E402
+from khdn_apps.direct_task_edit_button_patch import install as _install_v2314  # noqa: E402
 
 _install_v231(_app_module.__dict__)
 _install_v2311(_app_module.__dict__, _workload_patch_module)
 _install_v2312(_app_module.__dict__)
 _install_v2313(_app_module.__dict__)
+_install_v2314(_app_module.__dict__, _full_task_edit_module)
 _app_module.app()
