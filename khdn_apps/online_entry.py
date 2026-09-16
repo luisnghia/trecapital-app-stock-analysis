aa0d4003a29daf0e0251fcd6196b6b1665cf1ffe
luisnghia <nghia1214@gmail.com>
2026-09-16 15:40:13 +0700

    KHDN V2.31.4: install direct task edit button online

--- khdn_apps/online_entry.py
+++ khdn_apps/online_entry.py
@@ -13,13 +13,16 @@ os.environ.setdefault("KHDN_DB_PATH", str(DATA_DIR / "khdn_ops.db"))
 
 import khdn_apps.app as _app_module
 import khdn_apps.cbht_workload_patch as _workload_patch_module
+import khdn_apps.full_task_edit_patch as _full_task_edit_module
 from khdn_apps.cbht_workload_patch import install as _install_v231
 from khdn_apps.leader_workload_match_patch import install as _install_v2311
 from khdn_apps.amount_decimal_patch import install as _install_v2312
 from khdn_apps.full_task_edit_patch import install as _install_v2313
+from khdn_apps.direct_task_edit_button_patch import install as _install_v2314
 
 _install_v231(_app_module.__dict__)
 _install_v2311(_app_module.__dict__, _workload_patch_module)
 _install_v2312(_app_module.__dict__)
 _install_v2313(_app_module.__dict__)
+_install_v2314(_app_module.__dict__, _full_task_edit_module)
 _app_module.app()
