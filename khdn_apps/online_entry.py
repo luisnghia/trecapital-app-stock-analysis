d238522ec135f98b70be4e2c98c7ad830dc7eb76
luisnghia <nghia1214@gmail.com>
2026-09-16 14:59:02 +0700

    KHDN V2.31.2: install decimal amount fix online

--- khdn_apps/online_entry.py
+++ khdn_apps/online_entry.py
@@ -15,7 +15,9 @@ import khdn_apps.app as _app_module
 import khdn_apps.cbht_workload_patch as _workload_patch_module
 from khdn_apps.cbht_workload_patch import install as _install_v231
 from khdn_apps.leader_workload_match_patch import install as _install_v2311
+from khdn_apps.amount_decimal_patch import install as _install_v2312
 
 _install_v231(_app_module.__dict__)
 _install_v2311(_app_module.__dict__, _workload_patch_module)
+_install_v2312(_app_module.__dict__)
 _app_module.app()
