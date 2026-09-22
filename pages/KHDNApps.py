8d2b7a88ad3b223a5df7a73328a7c3de400bf53c
luisnghia <nghia1214@gmail.com>
2026-09-22 17:32:44 +0700

    KHDN V2.32: install notification center in embedded page

--- pages/KHDNApps.py
+++ pages/KHDNApps.py
@@ -28,6 +28,7 @@ from khdn_apps.amount_decimal_patch import install as _install_v2312  # noqa: E4
 from khdn_apps.full_task_edit_patch import install as _install_v2313  # noqa: E402
 from khdn_apps.direct_task_edit_button_patch import install as _install_v2314  # noqa: E402
 from khdn_apps.catalog_sync_patch import install as _install_v2316  # noqa: E402
+from khdn_apps.notification_ui_patch import install as _install_v2320  # noqa: E402
 
 _install_v231(_app_module.__dict__)
 _install_v2311(_app_module.__dict__, _workload_patch_module)
@@ -35,4 +36,5 @@ _install_v2312(_app_module.__dict__)
 _install_v2313(_app_module.__dict__)
 _install_v2314(_app_module.__dict__, _full_task_edit_module)
 _install_v2316(_app_module.__dict__)
+_install_v2320(_app_module.__dict__)
 _app_module.app()
