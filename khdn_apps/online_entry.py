f25bfd492b1fc04a609b760453acd04dc90af40a
luisnghia <nghia1214@gmail.com>
2026-09-25 16:25:55 +0700

    KHDN: wire operational administration navigation

--- khdn_apps/online_entry.py
+++ khdn_apps/online_entry.py
@@ -15,6 +15,7 @@ import khdn_apps.app as _app_module
 import khdn_apps.cbht_workload_patch as _workload_patch_module
 import khdn_apps.full_task_edit_patch as _full_task_edit_module
 import khdn_apps.weekly_plan as _weekly_plan_module
+import khdn_apps.customer_work_patch as _customer_work_patch_module
 from khdn_apps.cbht_workload_patch import install as _install_v231
 from khdn_apps.leader_workload_match_patch import install as _install_v2311
 from khdn_apps.amount_decimal_patch import install as _install_v2312
@@ -25,6 +26,7 @@ from khdn_apps.notification_ui_patch import install as _install_v2320
 from khdn_apps.weekly_plan_governance_patch import install as _install_weekly_governance
 from khdn_apps.weekly_plan_patch import install as _install_weekly_plan
 from khdn_apps.customer_work_patch import install as _install_customer_work
+from khdn_apps.operations_admin_nav_patch import install as _install_ops_admin_nav
 
 _install_v231(_app_module.__dict__)
 _install_v2311(_app_module.__dict__, _workload_patch_module)
@@ -36,4 +38,5 @@ _install_v2320(_app_module.__dict__)
 _install_weekly_governance(_weekly_plan_module, _app_module.__dict__.get("LOGGER"))
 _install_weekly_plan(_app_module.__dict__)
 _install_customer_work(_app_module.__dict__)
+_install_ops_admin_nav(_customer_work_patch_module, _app_module.__dict__)
 _app_module.app()
