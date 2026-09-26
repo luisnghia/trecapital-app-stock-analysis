15ff85490879994c594c64e304445058aff97cd3
luisnghia <nghia1214@gmail.com>
2026-09-26 10:06:00 +0700

    KHDN: wire potential customers into planning and System Admin

--- khdn_apps/online_entry.py
+++ khdn_apps/online_entry.py
@@ -15,6 +15,8 @@ import khdn_apps.app as _app_module
 import khdn_apps.cbht_workload_patch as _workload_patch_module
 import khdn_apps.full_task_edit_patch as _full_task_edit_module
 import khdn_apps.weekly_plan as _weekly_plan_module
+import khdn_apps.weekly_plan_ui as _weekly_plan_ui_module
+import khdn_apps.customer_work as _customer_work_module
 import khdn_apps.customer_work_patch as _customer_work_patch_module
 import khdn_apps.customer_work_ui as _customer_work_ui_module
 from khdn_apps.cbht_workload_patch import install as _install_v231
@@ -29,6 +31,7 @@ from khdn_apps.weekly_plan_patch import install as _install_weekly_plan
 from khdn_apps.customer_work_patch import install as _install_customer_work
 from khdn_apps.operations_admin_nav_patch import install as _install_ops_admin_nav
 from khdn_apps.priority_today_patch import install as _install_priority_today
+from khdn_apps.potential_customer_patch import install as _install_potential_customer
 
 _install_v231(_app_module.__dict__)
 _install_v2311(_app_module.__dict__, _workload_patch_module)
@@ -38,6 +41,16 @@ _install_v2314(_app_module.__dict__, _full_task_edit_module)
 _install_v2316(_app_module.__dict__)
 _install_v2320(_app_module.__dict__)
 _install_weekly_governance(_weekly_plan_module, _app_module.__dict__.get("LOGGER"))
+# Install before Weekly Plan/Customer Work navigation. Priority Today captures the
+# prospect-aware customer-work create form installed here.
+_install_potential_customer(
+    _app_module.__dict__,
+    _customer_work_module,
+    _customer_work_ui_module,
+    _weekly_plan_module,
+    _weekly_plan_ui_module,
+    _app_module.__dict__.get("LOGGER"),
+)
 _install_weekly_plan(_app_module.__dict__)
 _install_customer_work(_app_module.__dict__)
 _install_ops_admin_nav(_customer_work_patch_module, _app_module.__dict__)
